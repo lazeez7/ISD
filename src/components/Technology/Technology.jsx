1dd75b2e421f0cde4843c8782319075eab4088ab
lazeez7 <abdulazizsultan033@gmail.com>
2024-07-25 19:15:30 +0500

    test

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -23,19 +23,19 @@ const Technology = () => {
                 <p>Consul</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (2).png" alt="" />
                 <p>Valut</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (3).png" alt="" />
                 <p>Zabbix</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (4).png" alt="" />
                 <p>Graffana</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (5).png" alt="" />
                 <p>Prometheus</p>
               </div>
             </div>
@@ -45,35 +45,35 @@ const Technology = () => {
      data-aos-duration="1000">
             <div className="tech__card_1">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (6).png" alt="" />
                 <p>Kotlin</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
+                <img src="Rectangle 5174 (7).png" alt="" />
                 <p>Java</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Flutter</p>
-              </div>
-              <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (8).png" alt="" />
                 <p>React</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (9).png" alt="" />
+                <p>Flutter</p>
+              </div>
+              <div className="tech__detal">
+                <img src="Rectangle 5174 (10).png" alt="" />
                 <p>Android</p>
               </div>
             </div>
 
             <div className="tech__card_2">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Spring</p>
+                <img src="Rectangle 5174 (11).png" alt="" />
+                <p>MySQL</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
-                <p>Hibernate</p>
+                <img src="Rectangle 5174 (12).png" alt="" />
+                <p>PosgresSQL</p>
               </div>
             </div>
             </div>
@@ -84,36 +84,36 @@ const Technology = () => {
      data-aos-duration="1000">
             <div className="tech__card_1">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
+                <img src="Rectangle 5174 (13).png" alt="" />
+                <p>Spring</p>
+              </div>
+              <div className="tech__detal">
+                <img src="Rectangle 5174 (14).png" alt="" />
+                <p>Hibernate</p>
+              </div>
+              <div className="tech__detal">
+                <img src="Rectangle 5174 (15).png" alt="" />
                 <p>Redis</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
+                <img src="Rectangle 5174 (16).png" alt="" />
                 <p>Elastic</p>
-              </div>
-              <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Sspring</p>
-              </div>
-              <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
               </div>
               
             </div>
 
             <div className="tech__card_2">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (17).png" alt="" />
+                <p>Jenkins</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (18).png" alt="" />
+                <p>SonarQube</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (19).png" alt="" />
+                <p>Gradle</p>
               </div>
             </div>
             </div>
@@ -124,16 +124,16 @@ const Technology = () => {
      data-aos-duration="1000">
             <div className="tech__card_1">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (20).png" alt="" />
+                <p>AWS</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (21).png" alt="" />
+                <p>kubernets</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (22).png" alt="" />
+                <p>Docker</p>
               </div>
              
               
@@ -141,12 +141,12 @@ const Technology = () => {
 
             <div className="tech__card_2">
               <div className="tech__detal">
-                <img src="Rectangle 5174.png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (23).png" alt="" />
+                <p>RabbitMQ</p>
               </div>
               <div className="tech__detal">
-                <img src="Rectangle 5174 (1).png" alt="" />
-                <p>Nginx</p>
+                <img src="Rectangle 5174 (24).png" alt="" />
+                <p>Kafka</p>
               </div>
               
             </div>
